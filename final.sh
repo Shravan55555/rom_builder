5306efc4bb13f1a5e744a5191e999b425938f6ed
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-09-17 08:38:05 +0530

    Update final.sh

--- final.sh
+++ final.sh
@@ -9,8 +9,8 @@ export USE_CCACHE=1
 ccache -M 50G -F 0
 ccache -o compression=true
 ccache -z
-export BUILD_USERNAME="IndraDP"
-export BUILD_HOSTNAME="idev"
+export BUILD_USERNAME="SSK"
+export BUILD_HOSTNAME="SSK"
 export WITHOUT_CHECK_API=true
-lunch nad_juice-userdebug
+lunch lineage_RMX1901-userdebug
 mka nad -j8
