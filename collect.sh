e033ca190cae17e1a2f30d991bd8a24a2b457d81
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2024-07-09 10:50:04 +0530

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -12,8 +12,8 @@ ccache -z
 export BUILD_USERNAME="SSK"
 export BUILD_HOSTNAME="SSK"
 export WITHOUT_CHECK_API=true
-lunch lineage_RMX1901-userdebug
-m evolution &
+lunch aosp_RMX1901-userdebug
+make bacon -j$(nproc --all) &
 sleep 97m
 kill %1
 ccache -s
